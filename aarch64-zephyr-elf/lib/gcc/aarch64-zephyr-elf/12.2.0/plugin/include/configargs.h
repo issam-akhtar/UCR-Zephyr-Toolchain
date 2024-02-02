99d7a301cb4cfbe496b37ab92ed806acd87dfe0b
issam-akhtar <issam.akhtar@gmail.com>
2024-02-02 00:57:53 -0700

    purge

--- aarch64-zephyr-elf/lib/gcc/aarch64-zephyr-elf/12.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/Volumes/Workspace/build/.build/aarch64-zephyr-elf/src/gcc/configure --build=aarch64-build_apple-darwin20.6.0 --host=aarch64-build_apple-darwin20.6.0 --target=aarch64-zephyr-elf --prefix=/Volumes/Workspace/output/aarch64-zephyr-elf --exec_prefix=/Volumes/Workspace/output/aarch64-zephyr-elf --with-local-prefix=/Volumes/Workspace/output/aarch64-zephyr-elf/aarch64-zephyr-elf --with-headers=/Volumes/Workspace/output/aarch64-zephyr-elf/aarch64-zephyr-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='Zephyr SDK 0.16.4' --with-bugurl=https://github.com/zephyrproject-rtos/sdk-ng/issues --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/Volumes/Workspace/build/.build/aarch64-zephyr-elf/buildtools --with-mpfr=/Volumes/Workspace/build/.build/aarch64-zephyr-elf/buildtools --with-mpc=/Volumes/Workspace/build/.build/aarch64-zephyr-elf/buildtools --with-isl=/Volumes/Workspace/build/.build/aarch64-zephyr-elf/buildtools --enable-lto --enable-target-optspace --disable-nls --enable-multiarch --enable-languages=c,c++ --with-gnu-ld --with-gnu-as --enable-initfini-array";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
